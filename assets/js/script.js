4e247114ab742fe07169378afeee79bc4ddde48e
unknown <Abishli-Kamal>
2022-07-19 17:26:03 +0400

    19.07.2022

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,27 @@ var respMenuFilters=document.querySelector(".respMenuFilters")
 var main=document.getElementById("main");
 var reset=document.querySelector(".reset");
 var responsiveUl=document.querySelector(".responsiveUl");
+var UhdMain=document.querySelector(".UhdMain")
 
-
-var rentmenuIcon =document.getElementById("rentmenuIcon")
 var responsiveRentFilter=document.querySelector(".responsiveRentFilter");
 
 filterbtn.addEventListener("click" ,function(){
 filterAll.classList.toggle("dBlock");   
+
 });
 
 respMenu.addEventListener("click",function(){
     respMenuFilters.classList.toggle("dBlock");
-    main.classList.toggle("dNone");
     reset.classList.toggle("dBlock");
     responsiveUl.classList.toggle("dNone");
 });
 
-rentmenuIcon.addEventListener("click" ,function(){
-    console.log("test"); 
-    });
+
+    $(document).ready(function(){
+        $(".rentmenuicon").click(function(){
+            console.log("test");
+        })
+    })
 
 
 
