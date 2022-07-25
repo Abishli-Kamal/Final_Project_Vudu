41c3ec429b39acb4c67808c92067b6ef62debdb0
unknown <Abishli-Kamal>
2022-07-25 14:29:17 +0400

    Front End finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,52 @@
-var filterbtn =document.getElementById("filterBtn");
-var filterAll=document.querySelector(".filterAll");
-var respMenu=document.getElementById("respMenu");
-var respMenuFilters=document.querySelector(".respMenuFilters")
-var main=document.getElementById("main");
-var reset=document.querySelector(".reset");
-var responsiveUl=document.querySelector(".responsiveUl");
-var UhdMain=document.querySelector(".UhdMain")
+var filterbtn = document.getElementById("filterBtn");
+var filterAll = document.querySelector(".filterAll");
+var respMenu = document.getElementById("respMenu");
+var respMenuFilters = document.querySelector(".respMenuFilters")
+var main = document.getElementById("main");
+var reset = document.querySelector(".reset");
+var responsiveUl = document.querySelector(".responsiveUl");
+var UhdMain = document.querySelector(".UhdMain")
+const box = document.querySelectorAll(".footerToggle")
 
-var responsiveRentFilter=document.querySelector(".responsiveRentFilter");
 
-filterbtn.addEventListener("click" ,function(){
-filterAll.classList.toggle("dBlock");   
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
 
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+box.forEach(boxs => {
+    boxs.addEventListener("click", () => {
+        boxs.classList.toggle("active");
+    });
 });
-
-respMenu.addEventListener("click",function(){
+filterbtn.addEventListener("click", function () {
+    filterAll.classList.toggle("dBlock");
+});
+respMenu.addEventListener("click", function () {
     respMenuFilters.classList.toggle("dBlock");
     reset.classList.toggle("dBlock");
-    responsiveUl.classList.toggle("dNone");
 });
+function PasswordShowHidden() {
+    var input = document.getElementById("myInput")
+    var hidden = document.getElementById("hide1")
+    var show = document.getElementById("hide2")
+
+    if (input.type === 'password') {
+        input.type = "text";
+        show.style.display = "block";
+        hidden.style.display = "none";
+    }
+    else {
+        input.type = "password";
+        show.style.display = "none";
+        hidden.style.display = "block";
+    }
+}
 
 
-    $(document).ready(function(){
-        $(".rentmenuicon").click(function(){
-            console.log("test");
-        })
-    })
 
 
 
@@ -34,4 +55,11 @@ respMenu.addEventListener("click",function(){
 
 
 
-  
+
+
+
+
+
+
+
+
